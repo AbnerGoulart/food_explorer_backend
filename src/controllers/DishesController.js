19bb2a9ffe9b061e9fad32eef1566382a70a93f2
Abner Goulart <abnergoulart.10@gmail.com>
2024-11-12 08:14:06 -0300

    atualização da API separando o Controller por classes

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -1,42 +1,82 @@
 class DishesController {
   async get(request, response) {
       let dishes = {
-          "meals": [
-              {
-                  "photo": "prugna-pie.jpg",
-                  "title": "Prugna Pie",
-                  "price": 79.97
-              },
-              {
-                  "photo": "prugna-pie.jpg",
-                  "title": "Prugna Pie",
-                  "price": 79.97
-              }
-          ],
-          "main_dishes": [
-              {
-                  "photo": "prugna-pie.jpg",
-                  "title": "Prugna Pie",
-                  "price": 79.97
-              },
-              {
-                  "photo": "prugna-pie.jpg",
-                  "title": "Prugna Pie",
-                  "price": 79.97
-              }        
-          ],
-          "juices": [
-              {
-                  "photo": "prugna-pie.jpg",
-                  "title": "Prugna Pie",
-                  "price": 79.97
-              },
-              {
-                  "photo": "prugna-pie.jpg",
-                  "title": "Prugna Pie",
-                  "price": 79.97
-              }
-          ]
+          "meals": {
+            "title": "Salada",
+            "data": [
+                {
+                    "photo": "/salada.png",
+                    "title": "Salada Verde",
+                    "price": 29.90,
+                    "id": 1
+                },
+                {
+                    "photo": "salada2.png",
+                    "title": "Salada Italiana",
+                    "price": 29.90,
+                    "id": 2
+                },
+                {
+                    "photo": "/salada.png",
+                    "title": "Prugna Pie",
+                    "price": 79.97,
+                    "id": 7
+                }
+            ]
+          },
+          "main_dishes": {
+            "title": "Pratos Principais",
+            "data": [
+
+                {
+                    "photo": "/bacon.png",
+                    "title": "Bacon Nórdico",
+                    "price": 51.78,
+                    "id": 3
+                },
+                {
+                    "photo": "/queijo.png",
+                    "title": "Queijo Mineiro",
+                    "price": 46.70,
+                    "id": 4
+                },
+                {
+                    "photo": "/torta.png",
+                    "title": "Torta Pataquera",
+                    "price": 16.75,
+                    "id": 8
+                },
+                {
+                    "photo": "/cake2.png",
+                    "title": "Homer Cake",
+                    "price": 6.70,
+                    "id": 4
+                }   
+            ]
+          },
+          "juices": {
+            "title": "Bebidas",
+            "data": [
+                {
+                    "photo": "/iceTea.png",
+                    "title": "Chá Gelado",
+                    "price": 14.57,
+                    "id": 5
+                },
+                {
+                    "photo": "/iceTea2.png",
+                    "title": "Chá de Canela",
+                    "price": 13.24,
+                    "id": 6
+                },
+                {
+                    "photo": "/capuccino.png",
+                    "title": "Capuccino",
+                    "price": 9.80,
+                    "id": 4
+                }
+            ]
+          } 
       }
       
       response.status(200).json(dishes)
