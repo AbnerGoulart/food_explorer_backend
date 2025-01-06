586b3d0421364151b9f4069b0a12f537ee469d61
Abner Goulart <abnergoulart.10@gmail.com>
2025-01-06 20:56:07 -0300

    criação do SessionControler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 const { Router } = require('express')
 const DetailsController = require('../controllers/DetailsController')
 const DishesController = require("../controllers/DishesController")
+const SessionController = require("../controllers/SessionController")
 
 const routes = Router()
 
 const dishesController = new DishesController()
 const detailsController = new DetailsController()
+const sessionController = new SessionController()
 
 routes.get('/dishes', dishesController.get)
 routes.get('/dishes/details/:id', detailsController.get)
+routes.post('/session', sessionController.create)
 
 module.exports = routes
