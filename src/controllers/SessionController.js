02d0bc25046821365bc8c81b80c058cfcc924e1a
Abner Goulart <abnergoulart.10@gmail.com>
2025-01-09 16:31:30 -0300

    adição de permissionamento entre client e admin

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,7 +1,9 @@
 class SessionController {
   async create (request, response) {
     if(request.body.email === "abner@email.com" && request.body.password === "1234"){
-      return response.status(200).json({user: "Abner", token: "apsodksfgk"});
+      return response.status(200).json({user: "Abner", token: "apsodksfgk", type:"client"})
+    } else if(request.body.email === "admin@email.com" && request.body.password === "1234"){
+      return response.status(200).json({user:"Admin", token:"oiwer89h", type:"admin"})
     } else {
       return response.status(500).json({message: "Usuário não autenticado"})
     }
